009fb57cdcfd2bcf0b1bcca1b2256af5327e17bf
backendCoderHouse <cjrios.info+profesor@gmail.com>
2023-04-27 14:22:38 -0300

    upload files

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// importando express
+const express = require("express");
+
+// destruturación de Server socket.
+const  { Server } = require("socket.io");
+
+// importando handlebars
+const handlebars = require('express-handlebars');
+
+// importamos el router
+const viewsRouter = require ('./routes/views.router.js');
+
+// express
+const app = express();
+const PORT = process.env.PORT||8080;
+const server = app.listen(PORT,()=>console.log("Listening on 8080"));
+
+app.engine('handlebars',handlebars.engine());
+app.set('views',__dirname+'/views');
+app.set('view engine','handlebars');
+
+app.use(express.json());
+app.use(express.static(__dirname+'/public'))
+
+app.use('/',viewsRouter);
+
+const io = new Server(server);
+const messages= [];
+
+io.on('connection',socket=>{
+    console.log("Socket connected");
+
+    socket.on('message',data=>{
+        messages.push(data);
+        io.emit('messageLogs',messages);
+    })
+    
+    socket.on('authenticated',data=>{
+        socket.broadcast.emit('newUserConnected',data);
+    })
+})
